513a76a58f8d66db27dc32a68e0d8572070c3d0b
NatanaelSz <natanaelsouza.bat@gmail.com>
2024-06-27 15:54:08 -0300

    Reformulando o codigo utilizando Switch Case

--- src/SistemaMedida.java
+++ src/SistemaMedida.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class SistemaMedida {
     public static void main(String[] args) {
-        String sigla = "G";
+        Scanner sc = new Scanner(System.in);
 
-        if (sigla == "P") {
-            System.out.println("Pequeno");
-        } else if (sigla == "M") {
-            System.out.println("Medio");
-        } else if (sigla == "G") {
-            System.out.println("Grande");
-        } else {
-            System.out.println("INDEFINIDO");
+        char sigla;
+        sigla = sc.next().toUpperCase().charAt(0);
+
+        switch (sigla) {
+            case 'P': {
+                System.out.println("Pequeno");
+                break;
+            }
+            case 'M': {
+                System.out.println("Medio");
+                break;
+            }
+            case 'G': {
+                System.out.println("Grande");
+                break;
+            }
+            default:
+                System.out.println("Indefinido");
         }
+
+        sc.close();
     }
 }
