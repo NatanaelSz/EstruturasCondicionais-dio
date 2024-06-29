45d5dae6ddd1f7f48475e29affa4bc18d61789ab
Natanael Souza <77692209+NatanaelSz@users.noreply.github.com>
2024-06-29 15:40:26 -0300

    DoWhile

---
+++ src/EstruturasDeRepeticao/ExemploDoWhile.java
@@ -0,0 +1,21 @@
+package EstruturasDeRepeticao;
+
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando ....");
+
+        do {
+            System.out.println("Telefone Tocando");
+        } while (tocando()); {
+            System.out.println("Alôôô!");
+        }
+
+    }
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3) == 1;
+        System.out.println("Atendeu? " + atendeu);
+        return ! atendeu;
+    }
+}
