75b62a0c247fb8d775cdf5cb90849a88b62c47e0
Natanael Souza <77692209+NatanaelSz@users.noreply.github.com>
2024-07-09 11:43:56 -0300

    ATV - Fazendo ligações para candidatos selecionados

--- src/Candidatura/ProcessoSeletivo.java
+++ src/Candidatura/ProcessoSeletivo.java
@@ -1,10 +1,40 @@
 package Candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentado = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentado = !atendeu;
+            if (continuarTentado) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato Realizado com Sucesso");
+            }
+        } while (continuarTentado && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.printf("Conseguimos contato com %s na %d tentativa\n", candidato, tentativasRealizadas);
+        } else {
+            System.out.printf("Não conseguimos contato com %s, número maximo tentativas %d realizadas\n", candidato, tentativasRealizadas);
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static void imprimirSelecionados() {
